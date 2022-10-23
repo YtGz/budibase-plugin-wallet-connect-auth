05ecda36a90b297e112d00a64c217d916c7da33a
YtGz <10178241+YtGz@users.noreply.github.com>
2022-10-23 21:27:05 +0200

    feat: implement walletconnect auth SDK

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,16 +1,67 @@
 <script>
   import { getContext } from 'svelte'
+  import { AuthClient, generateNonce } from '@walletconnect/auth-client'
+  import QRCodeModal from '@walletconnect/qrcode-modal'
 
   const { styleable, Provider } = getContext('sdk')
   const component = getContext('component')
 
-  let web3Provider = undefined
+  export let walletConnectCloudProjectId
+  export let dAppName
+  export let dAppDescription = ''
+  export let dAppDomain
+  export let dAppUrl
+  export let dAppIcon = ''
+  export let dAppChainId = 'eip155:1'
+
+  let isAuthenticated = false
   $: dataContext = {
-    web3Provider
+    isAuthenticated
+  }
+
+  const authClientPromise = AuthClient.init({
+    projectId: walletConnectCloudProjectId,
+    metadata: {
+      name: dAppName,
+      description: dAppDescription,
+      url: dAppUrl,
+      icons: [dAppIcon]
+    }
+  })
+
+  const authenticate = async () => {
+    const authClient = await authClientPromise
+    const nonce = generateNonce()
+    const { uri } =
+      (await authClient.request({
+        aud: dAppUrl,
+        domain: dAppDomain,
+        chainId: dAppChainId,
+        nonce: nonce
+      })) ?? undefined
+
+    if (uri !== undefined) {
+      QRCodeModal.open(uri)
+    }
+
+    authClient.on('auth_response', ({ params }) => {
+      if (Boolean(params.result?.s)) {
+        // Response contained a valid signature -> user is authenticated.
+        console.log('Authentication successful.')
+        isAuthenticated = true
+        QRCodeModal.close()
+      } else {
+        // Handle error or invalid signature case
+        console.error(params.message)
+      }
+    })
   }
 </script>
 
 <div use:styleable={$component.styles}>
+  {#await authClientPromise then}
+    <button on:click={authenticate}>Connect Wallet</button>
+  {/await}
   <Provider data={dataContext}>
     <slot />
   </Provider>
